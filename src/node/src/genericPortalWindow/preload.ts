544062b376ca4e73b9d74b557ff2b39616cf11dd
Cyrus Roshan <cyrusroshan@users.noreply.github.com>
2021-08-24 22:13:04 -0700

    Better preload export format.

--- src/node/src/genericPortalWindow/preload.ts
+++ src/node/src/genericPortalWindow/preload.ts
@@ -2,19 +2,21 @@ import { ipcRenderer } from 'electron'
 
 import { WindowIpcTopic } from '@portal-windows/core'
 
-window.electronPublish = (msg: WindowIpcTopic, ...args) => {
-  try {
-    ipcRenderer.send(msg, ...args)
-  } catch (e) {
-    console.warn(e)
+export function preload() {
+  window.electronPublish = (msg: WindowIpcTopic, ...args) => {
+    try {
+      ipcRenderer.send(msg, ...args)
+    } catch (e) {
+      console.warn(e)
+    }
   }
-}
 
-window.electronSubscribe = (channel: WindowIpcTopic, listener: any) => {
-  ipcRenderer.on(channel, listener)
-}
+  window.electronSubscribe = (channel: WindowIpcTopic, listener: any) => {
+    ipcRenderer.on(channel, listener)
+  }
 
-window.electronUnsubscribe = (channel: WindowIpcTopic, func?: (...args: any[]) => void) => {
-  if (func) ipcRenderer.removeListener(channel, func)
-  else ipcRenderer.removeAllListeners(channel)
+  window.electronUnsubscribe = (channel: WindowIpcTopic, func?: (...args: any[]) => void) => {
+    if (func) ipcRenderer.removeListener(channel, func)
+    else ipcRenderer.removeAllListeners(channel)
+  }
 }
